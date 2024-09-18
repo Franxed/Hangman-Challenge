3eb4902253522926e46c69a290157c2eff9a38c3
Franx <fe46315@gmail.com>
2024-09-18 14:41:21 +0200

    Finished Project, Not visually displayed. Used 6 - chance counter.

--- hangman.py
+++ hangman.py
@@ -1,16 +1,42 @@
+# Import libraries.
 import random
 
+# Created customized list.
 list_words = ['hangman', 'apple', 'computer', 'satellite', 'table', 'book', 'happiness']
-random_word = random.choice(list_words).upper()
-print(random_word)
-random_word = "_" * len(random_word)
-count = 0
+random_word = random.choice(list_words).upper()  # Randomly picks from the list.
+print(random_word)  # Testing (optional)
 
-for letter in random_word:
-    print(letter, end=" ")
+count = 0   # Start count for wrong guesses.
+
+# Initialize the blank line.
+placeholder = "_" * len(random_word)
+print(placeholder)  # Shows the initial blank placeholder.
 
 while True:
-    guess = input("\nGuess:").upper()
+    display = ""
+    guess = input("Guess : ").upper()
 
-    if guess == random_word:
-        pass
+    # Check if the guessed letter is in the word and update the placeholder.
+    for letter in range(len(random_word)):
+        if random_word[letter] == guess:
+            display += random_word[letter]  # Add guessed letter at the correct position.
+        else:
+            display += placeholder[letter]
+
+    placeholder = display        # Update the placeholder with the new display.
+    print(placeholder)           # Display the updated word with guessed letters.
+
+    # Checks if the guess is wrong.
+    if guess not in random_word:
+        count += 1
+        print("Wrong guess. You have ", 6 - count, " tries left.")
+
+    # Checks if all letter have been guessed.
+    if "_" not in placeholder:
+        print("Congratulations! You won!")
+        break
+
+    # Gives 6 chances.
+    if count >= 6:
+        print(f"Hanged! The word was {random_word}.\n Better luck next time!")
+        break
